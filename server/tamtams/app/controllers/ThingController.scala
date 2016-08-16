b4e972be2bb0a49eb191d6a2ae3fca38088cbee5
antonin pa <antonin.perrotaudet@orange.com>
2016-08-16 19:15:24 +0200

    on going transfo

--- server/tamtams/app/controllers/ThingController.scala
+++ server/tamtams/app/controllers/ThingController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+
 import akka.util.ByteString
 import play.api.Logger
 import play.api.http.HttpEntity
@@ -9,17 +10,67 @@ import play.api.mvc._
 import logic.JsonConversion._
 import logic.Thing
 import logic.ThingsGenerator
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.bson.BSONDocument
+import reactivemongo.play.json.collection.JSONCollection
+
+import scala.concurrent.{ExecutionContext, Future}
 
 
 /**
   * This controller creates Actions to handle HTTP requests
   * related to [[Thing]]
+  * it connects to a MongoDb database to serve requests
   */
 @Singleton
-class ThingController @Inject() extends Controller {
+class ThingController @Inject() (val reactiveMongoApi: ReactiveMongoApi)
+                                (implicit exec: ExecutionContext)
+  extends Controller with MongoController with ReactiveMongoComponents {
 
 
-// TODO : test and return URI with created status
+
+  // connect to local database using [[database]] and
+  // to TamtamThings collection as a [[reactivemongo.api.Collection]]
+  val thingsCollection : Future[JSONCollection] =
+    database.map( // once future database is completed :
+      connectedDb => connectedDb.collection[JSONCollection]("TamtamThings")
+    )
+
+  // todo : put this code in a function taking a list of anything having an id and returning json or notfound
+  def getThing(thingId: String) = Action.async {
+    request => {
+
+      val findThingQuery = BSONDocument
+      // todo  :define find query
+
+      val dbWriteResult : Future[WriteResult] = for {
+        resolvedCollection <- thingsCollection
+        lastError <- resolvedCollection.find()
+      }yield ???
+???
+
+
+
+    /*
+    val foundThing: Option[Thing] = ThingsGenerator.thingSeq.find(thing => thing.id == thingId)
+    if (foundThing.isDefined) {
+      val jsonThing = Json.toJson(foundThing.get)
+      val jsonString = jsonThing.toString
+      Logger.info(s"tamtams : Thing -> JSON : ${jsonString} ")
+      Result(
+        header = ResponseHeader(OK, Map.empty),
+        body = HttpEntity.Strict(ByteString(jsonString), Some("application/json"))
+      )
+    } else {
+      Logger.info(s"tamtams : ${thingId} Not found ")
+      NotFound
+    }*/
+    }
+  }
+
+
+  // TODO : test and return URI with created status
   /**
     * As of today this action can take a Json as a parameter
     * and parse it into a [[Thing]] object.
@@ -34,22 +85,6 @@ class ThingController @Inject() extends Controller {
   }
 
 
-  // todo : put this code in a function taking a list of anything having an id and returning json or notfound
-  def getThing(thingId: String) = Action {
-    val foundThing: Option[Thing] = ThingsGenerator.thingSeq.find(thing => thing.id == thingId)
-    if (foundThing.isDefined) {
-      val jsonThing = Json.toJson(foundThing.get)
-      val jsonString = jsonThing.toString
-      Logger.info(s"tamtams : Thing -> JSON : ${jsonString} ")
-      Result(
-        header = ResponseHeader(OK, Map.empty),
-        body = HttpEntity.Strict(ByteString(jsonString), Some("application/json"))
-      )
-    } else {
-      Logger.info(s"tamtams : ${thingId} Not found ")
-      NotFound
-    }
-  }
 
   // todo : get things near with database near functions
   def getThingsNear(lat: Double, lon: Double) = TODO
